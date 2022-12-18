e0c3c213eee434170011a7cd5599b12c9e9f8f07
MedoGhoz <ghozmedo@gmail.com>
2022-12-18 14:15:09 +0200

    sentence generator using python

--- src/controllers/textToSpeechController.js
+++ src/controllers/textToSpeechController.js
@@ -2,20 +2,37 @@
 // var path = require('path');
 import gtts from "node-gtts";
 import path from "path";
+import {spawn, spawnSync} from "child_process";
 import express from "express";
 var g = gtts("en");
-var filepath = path.join(__dirname+"/../audio", 'i-love-you.mp3');
+var filepath = path.join(__dirname + "/../audio", 'audio.mp3');
 
 
-export function GetAudio(req,res){
-  g.save(filepath, req.params.text, function() {
-    console.log(req.params.text+' save done');
-    res.sendFile(path.resolve('audio/i-love-you.mp3'), "/src/audio/i-love-you.mp3", function (err) {
-      if (err) {
+export function GetAudio(req, res) {
+  req.range(10000000000);
+  let text;
+  const python = spawn('python', [path.resolve('src/python/sentenceGenerator.py')]);
+  
+
+  python.stdout.on('data', function (data)  {
+
+    text = data.toString();
+    console.log(text);
+    g.save(filepath, text, function () {
+      console.log(text + ' save done');
+      res.sendFile(path.resolve('src/audio/audio.mp3'), "/src/audio/audio.mp3", function (err) {
+        if (err) {
           console.log(err);
-      } else {
+        } else {
           console.log('Sent:', filepath);
-      }
+        }
+      });
+    });
   });
-  }); 
+
+  python.on('close', (data) => {
+    console.log(data.toString());
+    console.log("script closed...");
+  });
+
 } 
---
+++ src/python/sentenceGenerator.py
@@ -0,0 +1,4 @@
+from wonderwords import RandomSentence
+
+sentence = RandomSentence()
+print(sentence.sentence())
