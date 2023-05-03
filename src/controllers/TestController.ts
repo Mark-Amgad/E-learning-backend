020a8f152ae509d59dae9ecbde41ad57b1102adf
Mark Amgad <64510804+Mark-Amgad@users.noreply.github.com>
2023-05-03 18:17:04 +0300

    Test schema

---
+++ src/controllers/TestController.ts
@@ -0,0 +1,54 @@
+import { TestModel } from "../models/Test";
+import QuestionModel from "../models/Question";
+import { Request, Response } from "express";
+
+export class TestController
+{
+    async createTest(req:Request,res:Response)
+    {
+        try
+        {
+
+        }
+        catch(err)
+        {
+
+        }
+    }
+
+    async getTest(req:Request,res:Response)
+    {
+        try
+        {
+
+        }
+        catch(err)
+        {
+            
+        }
+    }
+
+    async submitTest(req:Request,res:Response)
+    {
+        try
+        {
+
+        }
+        catch(err)
+        {
+            
+        }
+    }
+
+    async deleteTest(req:Request,res:Response)
+    {
+        try
+        {
+
+        }
+        catch(err)
+        {
+            
+        }
+    }
+}
