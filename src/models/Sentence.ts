2e2994742707d016942c94ecfcfbace7245dfc85
MedoGhoz <ghozmedo@gmail.com>
2023-05-10 17:09:56 +0300

    generate listening question
    modified the listening question to fetch and save to database

--- src/models/Sentence.ts
+++ src/models/Sentence.ts
@@ -4,11 +4,13 @@ export interface Sentence
 {
     text:string,
     level:string,
+    tenses:string[],
 }
 
 const SentenceSchema = new Schema<Sentence>({
     text:{type:String, required:true},
     level:{type:String, required:true},
+    tenses:{type:[String], required:true},
 });
 
 
