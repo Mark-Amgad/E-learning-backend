577084fa3e586105ef8e1021c116491df52edd53
MedoGhoz <ghozmedo@gmail.com>
2023-06-16 19:03:18 +0300

    listening demo

--- src/routes/TextToSpeechRouter.ts
+++ src/routes/TextToSpeechRouter.ts
@@ -1,8 +1,9 @@
-import {GetAudio} from "../controllers/textToSpeechController";
+import {GetAudio, audio} from "../controllers/textToSpeechController";
 import express from "express";
 const textToSpeechRouter = express.Router();
 
 export default textToSpeechRouter;
 
 
+textToSpeechRouter.get("/audio/getAudio",audio);
 textToSpeechRouter.get("/:level",GetAudio);
